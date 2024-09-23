ae8f11952af1f154a7b5f84a76db1beb45d9f4f5
AlexeyTarasov77 <alexxey2707@gmail.com>
2024-09-23 19:30:25 +0200

    Added two more functions (GetUser and NewActivationToken) to sso grpc gateway

--- internal/clients/sso/grpc/grpc.go
+++ internal/clients/sso/grpc/grpc.go
@@ -12,15 +12,18 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 )
 
 type Client struct {
-	api       ssov1.AuthClient
-	log       *slog.Logger
-	appId     int32
+	api   ssov1.AuthClient
+	log   *slog.Logger
+	appId int32
 }
 
-/* New creates a new Client instance.
+/*
+	New creates a new Client instance.
+
 It takes a context, a logger, an address of the gRPC server, a timeout for retry call,
 and a retries count as parameters.
 Returns a Client instance and an error.
@@ -52,41 +55,91 @@ func New(
 		return nil, err
 	}
 	return &Client{
-		api:       ssov1.NewAuthClient(cc),
-		log:       log,
-		appId:     appId,
+		api:   ssov1.NewAuthClient(cc),
+		log:   log,
+		appId: appId,
 	}, nil
 }
 
 func (c *Client) IsAdmin(ctx context.Context, userID int64) (bool, error) {
+	const op = "grpc.Client.GetUser"
+	log := c.log.With("op", op)
 	resp, err := c.api.IsAdmin(ctx, &ssov1.IsAdminRequest{UserId: userID})
 	if err != nil {
-		c.log.Error("Error calling Client.IsAdmin", "errMsg", err.Error())
+		log.Error("Error", "errMsg", err.Error())
 		return resp.GetIsAdmin(), err
 	}
 	return resp.GetIsAdmin(), nil
 }
 
-
-func (c *Client) Login(ctx context.Context, email, password string) (*auth.Tokens, error) {
+func (c *Client) Login(ctx context.Context, email, password string) (*auth.TokensDTO, error) {
+	const op = "grpc.Client.GetUser"
+	log := c.log.With("op", op)
 	resp, err := c.api.Login(ctx, &ssov1.LoginRequest{Email: email, Password: password, AppId: c.appId})
 	if err != nil {
-		c.log.Error("Error calling Client.Login", "errMsg", err.Error())
+		log.Error("Error", "errMsg", err.Error())
 		return nil, err
 	}
-	return &auth.Tokens{AccessToken: resp.GetAccessToken(), RefreshToken: resp.GetRefreshToken()}, nil
+	return &auth.TokensDTO{AccessToken: resp.GetAccessToken(), RefreshToken: resp.GetRefreshToken()}, nil
 }
 
 func (c *Client) Register(ctx context.Context, email, username, password string) (*auth.SignupData, error) {
+	const op = "grpc.Client.GetUser"
+	log := c.log.With("op", op)
 	resp, err := c.api.Register(
 		ctx,
 		&ssov1.RegisterRequest{Email: email, Password: password, Username: username},
 	)
 	if err != nil {
-		c.log.Error("Error calling Client.Register", "errMsg", err.Error())
+		log.Error("Error", "errMsg", err.Error())
 		return nil, err
 	}
 	return &auth.SignupData{UserID: resp.GetUserId(), ActivationToken: resp.GetActivationToken()}, nil
+}
+
+func (c *Client) GetUser(ctx context.Context, params auth.GetUserParams) (*auth.UserDTO, error) {
+	const op = "grpc.Client.GetUser"
+	log := c.log.With("op", op)
+	resp, err := c.api.GetUser(ctx, &ssov1.GetUserRequest{Id: params.ID, Email: params.Email, IsActive: params.IsActive})
+	if err != nil {
+		grpcErr, ok := status.FromError(err)
+		if ok {
+			switch grpcErr.Code() {
+			case codes.NotFound:
+				return nil, auth.ErrUserNotFound
+			case codes.InvalidArgument:
+				return nil, auth.ErrInvalidData.SetMessage(grpcErr.Message())
+			}
+		}
+		log.Error("Error", "errMsg", err.Error())
+		return nil, err
+	}
+	user := resp.GetUser()
+	const timeParseLayout = "2006-01-02 15:04:05.999999 -0700 MST"
+	createdAt, err := time.Parse(timeParseLayout, resp.GetUser().GetCreatedAt())
+	if err != nil {
+		return nil, err
+	}
+	updatedAt, err := time.Parse(timeParseLayout, resp.GetUser().GetUpdatedAt())
+	if err != nil {
+		return nil, err
+	}
+	return &auth.UserDTO{
+		ID: user.GetId(), Email: user.GetEmail(),
+		Username: user.GetUsername(), IsActive: user.GetIsActive(),
+		CreatedAt: createdAt, UpdatedAt: updatedAt,
+	}, nil
+}
+
+func (c *Client) NewActivationToken(ctx context.Context, email string) (string, error) {
+	const op = "grpc.Client.NewActivationToken"
+	log := c.log.With("op", op)
+	resp, err := c.api.NewActivationToken(ctx, &ssov1.NewActivationTokenRequest{Email: email})
+	if err != nil {
+		log.Error("Error", "errMsg", err.Error())
+		return "", err
+	}
+	return resp.GetActivationToken(), nil
 }
 
 // func (c *Client) ActivateUser(ctx context.Context, token string) (bool, error) {
---
+++ internal/services/auth/errors.go
@@ -0,0 +1,21 @@
+package auth
+
+import "errors"
+
+type errInvalidData struct {
+	msg string
+}
+
+func (e *errInvalidData) SetMessage(msg string) error {
+	e.msg = msg
+	return e
+}
+
+func (e *errInvalidData) Error() string {
+	return e.msg
+}
+
+var (
+	ErrUserNotFound = errors.New("user not found")
+	ErrInvalidData = &errInvalidData{}
+)
